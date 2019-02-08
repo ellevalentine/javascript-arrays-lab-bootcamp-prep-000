41871f936d671d48513760f3b049a08acd91af89
classy-debugger-3131 <elle.valentine@hotmail.com>
2019-02-08 06:41:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function appendKitten(name){ return [...kittens, "Broom" ];}
  
 function prependKitten(name){ return [ "Arnold", ...kittens ];}
   
-function removeLastKitten(kittens){ return kittens.slice(0, 2);}
+function removeLastKitten(kittens){ return kittens.splice(2, 1);}
 
 function removeFirstKitten(kittens){ return kittens.slice(1);}
 
